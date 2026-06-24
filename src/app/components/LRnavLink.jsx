e181fe99a4d0ad053d525317d02b7971f0069f65
shuvro18 <shuvrobiswas404@gmail.com>
2026-06-24 12:27:28 +0600

    solve the buttons movement

--- src/app/components/LRnavLink.jsx
+++ src/app/components/LRnavLink.jsx
@@ -9,7 +9,7 @@ const LRnavLink = ({children, href}) => {
     const isTrue = pathName === href ;
 
     return (
-        <Link href={href} className={`${isTrue ?  "px-4 py-2 rounded-md font-medium text-sm text-white bg-[#002244]" : " text-gray-600 px-4 py-2 text-sm font-medium  transition-colors"}`}>
+        <Link href={href} className={`hover:border-b-2  hover:border-[#002244] hover:rounded-md  hover:py-1 ${isTrue ?  "px-4 py-2 rounded-md font-medium text-sm text-white bg-[#002244]" : " text-gray-600 px-4 py-2 text-sm font-medium  transition-colors"}`}>
             {children}
         </Link>
     );
