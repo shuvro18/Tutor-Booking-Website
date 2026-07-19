269908c7c9a6eace9f1fdbdd6e0b928ecfd27597
shuvro18 <shuvrobiswas404@gmail.com>
2026-07-19 22:40:17 +0600

    delete api made

--- src/app/lib/actions.js
+++ src/app/lib/actions.js
@@ -1,17 +1,24 @@
-
 import { redirect } from "next/navigation";
 
-export const createUser = async(newUser)=>{
-   
-    const res = await fetch("http://localhost:5000/users",{
-        method: "POST",
-        headers: {
-            "Content-type": "application/json"
-        },
-        body: JSON.stringify(newUser)
+export const createUser = async (newUser) => {
+  const res = await fetch("http://localhost:5000/users", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(newUser),
+  });
+  const data = await res.json();
+
+  return data;
+};
+
+export const deleteUserData = async (userId) => {
+    'use server';
+    const res = await fetch(`http://localhost:5000/users/${userId}`,{
+        method:'DELETE'
     })
     const data = await res.json();
+        return data;
     
-    return data 
-}
-
+};
